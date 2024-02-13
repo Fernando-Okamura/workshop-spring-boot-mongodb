77d2f7c63118be0cf784f9002da137436485a7bd
Fernando Okamura <fokamura10@gmail.com>
2024-02-13 16:59:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fernandookamura/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.fernandookamura.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fernandookamura.workshopmongo.domain.Post;
+import com.fernandookamura.workshopmongo.repository.PostRepository;
+import com.fernandookamura.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+}
+}
