4d88c2401f613407e83f2d283f5ab896a3ebc635
Fernando Okamura <fokamura10@gmail.com>
2024-02-13 16:42:28 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/fernandookamura/workshopmongo/resources/UserResource.java
+++ src/main/java/com/fernandookamura/workshopmongo/resources/UserResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fernandookamura.workshopmongo.domain.Post;
 import com.fernandookamura.workshopmongo.domain.User;
 import com.fernandookamura.workshopmongo.dto.UserDTO;
 import com.fernandookamura.workshopmongo.services.UserService;
@@ -31,6 +39,7 @@ public class UserResource {
 	User obj = service.findById(id);
 	return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+		
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 	User obj = service.fromDTO(objDto);
@@ -50,6 +59,11 @@ public class UserResource {
 	obj = service.update(obj);
 	return ResponseEntity.noContent().build();
 }
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+	User obj = service.findById(id);
+	return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
 	
 	
